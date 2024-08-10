b2efc5d00b7fe4d861b835b4be2c948d5e11a1b4
twozero28 <28twozero@gmail.com>
2024-08-10 21:24:39 +0900

    feat. added auto text effect animation

---
+++ auto-text-effect-animation/js/index.js
@@ -0,0 +1,24 @@
+const identities = ["Web developer", "Full-stack developer", "looking for a job"];
+
+const contentEl = document.getElementById("content");
+
+
+
+let identitiesIndex = 0;
+let characterIndex = 0;
+
+const updateText = () => {
+    characterIndex++;
+    contentEl.innerText = `I am a ${identities[identitiesIndex].slice(0, characterIndex)}`
+    if (characterIndex === identities[identitiesIndex].length) {
+        characterIndex = 0;
+        identitiesIndex++;
+
+    }
+    if (identitiesIndex === identities.length) {
+        identitiesIndex = 0
+    }
+    setTimeout(updateText, 300);
+}
+
+updateText();
